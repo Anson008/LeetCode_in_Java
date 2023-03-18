84005c7ef51fa4e8d347efd70ac4288f1dda253e
Xingchen Zhao <xczhao007@gmail.com>
2023-03-17 20:44:29 -0600

    Add LC121 Best Time to Buy and Sell Stock

---
+++ src/dsa/DynamicProgramming/LC121.java
@@ -0,0 +1,24 @@
+package dsa.DynamicProgramming;
+
+public class LC121 {
+    public int maxProfit(int[] prices){
+        final int n = prices.length;
+        if (n == 1)
+            return 0;
+
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < n; i++){
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        }
+
+        return dp[n - 1][0];
+    }
+
+    public void runTest(int[] prices){
+        System.out.println(maxProfit(prices));
+    }
+}
