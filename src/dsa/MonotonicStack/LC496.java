81d973eeb6c89c69260c655f925e88cf296029e6
Xingchen Zhao <xczhao007@gmail.com>
2024-02-26 15:11:44 -0700

    Add LC496 "Next Greater Element I"

---
+++ src/dsa/MonotonicStack/LC496.java
@@ -0,0 +1,22 @@
+package dsa.MonotonicStack;
+
+import java.util.*;
+
+public class LC496 {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = nums2.length - 1; i >= 0; i--){
+            while(!stack.isEmpty() && nums2[i] >= stack.peek())
+                stack.pop();
+            map.put(nums2[i], stack.isEmpty() ? -1 : stack.peek());
+            stack.push(nums2[i]);
+        }
+
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++){
+            res[i] = map.get(nums1[i]);
+        }
+        return res;
+    }
+}
